515741c7c3acc422e42d4ca42e825b26c2b67daf
Vinayak96k <vinayak.patil0304@gmail.com>
2018-08-29 15:39:55 +0530

    first commit

---
+++ EntryPoint.c
@@ -0,0 +1,12 @@
+#include"MyHeader.h"
+
+int main(int argc, char const *argv[])
+{
+    int iValue=0;
+    printf("Enter a number:");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
---
+++ Helper.c
@@ -0,0 +1,22 @@
+#include"MyHeader.h"
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//  Function Name: Display
+//  Description: Accepts one number and prints Hello if number is less than 10 ortherwise Demo
+//  Input: int[IN]
+//  Output: NA
+//  Author: Vinayak Mahendra Patil
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+void Display(int iNo)
+{
+if(iNo<10)
+{
+printf("Hello\n");
+}
+else
+{
+printf("Demo\n");
+}
+
+}
